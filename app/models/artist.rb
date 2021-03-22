bc2d05905aa53c7ddd6dbdb204b11b3cb7b4838b
Nestor <nestor.venegas95@gmail.com>
2021-03-22 14:22:44 -0500

    commit tree a44e17fe8addf500d2595ed26a4d8caa7bf69861

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count
+        self.songs.count 
+    end
+
+    def scit
+        self.name+" has #{self.song_count} song"
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Song.destroy_all
+Artist.destroy_all
+
+a1 = Artist.create(name: "Galleta")
+a2 = Artist.create(name: "Mancha")
+
+s1 = Song.create(title: "Me-Mow", artist: a1)
+s2 = Song.create(title: "Meooow", artist: a2)
+s3 = Song.create(title: "Meruow", artist: a2)
+s4 = Song.create(title: "Prrrrr", artist: a1)
